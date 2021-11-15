72ec73f417a668fa2b4baed684302360fb5b8a69
dimabytes <dimabytes@gmail.com>
2021-11-15 15:52:34 +0300

    change timeout

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const telegram = new Telegram(process.env.BOT_TOKEN);
 const chatId = '340220499';
 const chat2Id = '914347880';
-const checkTimeout = 2500;
+    const checkTimeout = 1000;
 
 telegram.sendMessage(
     chatId,
